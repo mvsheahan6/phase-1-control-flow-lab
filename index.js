45dd3d0ab5b8dbb2834da76f8387f973fbb3194d
Matthew Sheahan <sheahan.matt@gmail.com>
2022-11-20 11:38:01 -0800

    commit tree a497a508b5da00ec49b1bfe304d2b758ba87831c

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet < 400) {
+    return "This one is on me!"
+  } else if (feet >= 400 && feet <= 2000) {
+    return "That will be twenty bucks."
+  } else if (feet > 2000 && feet <=2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (feet >= 2501) {
+    return "No can do."
+  };
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city === "NYC"
+    ? "Ok, sounds good."
+    : "No go."
+  }
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+     case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default: "not generous"
+      return "Bye."
+  }
 }
